e0aaee892e7ae38b9a5c6d420c17bc0592838e61
Jungwoo <108061510+jwson-automation@users.noreply.github.com>
2024-06-24 23:10:38 +0900

    add page

---
+++ lib/modules/core/views/signUp/SignUpPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SignUpPage extends ConsumerWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
