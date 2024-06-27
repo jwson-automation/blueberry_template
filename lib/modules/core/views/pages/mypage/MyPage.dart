dcc372eba5d1b67c34a6f71b23f0ca50652c93a3
JaeHo Song <thruthesky@gmail.com>
2024-06-27 17:42:47 +0800

    adding andorid & web platform

--- lib/modules/core/views/pages/mypage/MyPage.dart
+++ lib/modules/core/views/pages/mypage/MyPage.dart
@@ -323,7 +323,7 @@ Widget _buildLogin(BuildContext context, WidgetRef ref) {
 
               final actionCodeSettings = ActionCodeSettings(
                 /// 아래의 내용을 각 프로젝트이 Deeplink 로 변경
-                url: 'https://withcenter-test-5.firebaseapp.com',
+                url: 'https://withcenter-test-5.web.app/',
                 handleCodeInApp: true,
 
                 /// iOS 용
