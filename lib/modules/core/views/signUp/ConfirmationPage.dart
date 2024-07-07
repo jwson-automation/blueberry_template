19030a6d612b93c6c34f266e3d7af20f10fc63e3
Jungwoo <108061510+jwson-automation@users.noreply.github.com>
2024-07-07 17:16:45 +0900

    add create user feature

--- lib/modules/core/views/signUp/ConfirmationPage.dart
+++ lib/modules/core/views/signUp/ConfirmationPage.dart
@@ -3,6 +3,10 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../dto/UserDto.dart';
+import '../../providers/firebase/FirebaseAuthServiceProvider.dart';
+import '../../providers/firebase/FirebaseStoreServiceProvider.dart';
+import '../../utils/AppStrings.dart';
 import 'SignUpPage.dart';
 
 final signUpProvider = FutureProvider((ref) async {
@@ -52,7 +56,8 @@ class ConfirmationPage extends ConsumerWidget {
           SizedBox(height: 20),
           isLoading.when(
             data: (value) => ElevatedButton(
-              onPressed: onNext, // 페이지 종료
+              onPressed:
+                  signUp(email.state, password.state, name.state, context, ref),
               child: Text('가입하기'),
             ),
             loading: () => CircularProgressIndicator(),
@@ -61,5 +66,41 @@ class ConfirmationPage extends ConsumerWidget {
         ],
       ),
     );
+  }
+
+  signUp(email, password, name, context, ref) async {
+    try {
+      // 사용자 계정 생성
+      var userCredential = await ref
+          .read(firebaseAuthServiceProvider)
+          .signUpWithEmailPassword(email, password);
+
+      // 새로운 UserDTO 인스턴스 생성
+      UserDTO newUser = UserDTO(
+          userId: userCredential!.uid,
+          email: email,
+          name: name,
+          // 초기 이름 값, 필요에 따라 수정
+          age: 0,
+          // 초기 나이 값, 필요에 따라 수정
+          profileImageUrl: '',
+          // 초기 프로필 사진 URL, 필요에 따라 수정
+          createdAt: DateTime.now() // 계정 생성 날짜
+          );
+
+      // Firestore에 사용자 정보 저장
+      await ref.read(firebaseStoreServiceProvider).createUser(newUser);
+
+      Navigator.pop(context);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(AppStrings.signUpSuccessMessage)),
+      );
+    } catch (e) {
+      // 회원가입 실패 시, 에러 메시지 출력
+      print('회원가입 실패: $e');
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('회원가입 실패: $e'),
+      ));
+    }
   }
 }
