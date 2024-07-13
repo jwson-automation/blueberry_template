e78635d5045234d601b7bb3c4654759536272a80
Jungwoo <108061510+jwson-automation@users.noreply.github.com>
2024-07-13 20:40:03 +0900

    freezed 수정

--- lib/model/PersonModel.dart
+++ lib/model/PersonModel.dart
@@ -1,7 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-part '../generated/PersonModel.freezed.dart';
-part '../generated/PersonModel.g.dart';
+part 'generated/PersonModel.freezed.dart';
+part 'generated/PersonModel.g.dart';
 
 @freezed
 class PersonModel with _$PersonModel {
