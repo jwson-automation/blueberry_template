8781b81c900e7dd2a7a1e9c43dadfdef209f3b38
Jungwoo <108061510+jwson-automation@users.noreply.github.com>
2024-07-13 22:29:01 +0900

    add comment

--- lib/model/UserModel.dart
+++ lib/model/UserModel.dart
@@ -1,8 +1,22 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'generated/UserModel.freezed.dart';
-
 part 'generated/UserModel.g.dart';
+
+/**
+ * UserModel
+ *
+ * 유저의 정보를 담는 모델입니다.
+ *
+ * userId: 유저의 아이디
+ * name: 유저의 이름
+ * email: 유저의 이메일
+ * age: 유저의 나이
+ * profileImageUrl: 유저의 프로필 이미지 URL
+ * createdAt: 유저 정보가 생성된 시간
+ *
+ * @jwson-automation
+ */
 
 @freezed
 abstract class UserModel with _$UserModel {
