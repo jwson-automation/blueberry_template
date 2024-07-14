4bb3378f0455f540836cb0412afa156cd6fdb9c3
Jungwoo <108061510+jwson-automation@users.noreply.github.com>
2024-07-14 16:15:55 +0900

    stf -> stl

---
+++ lib/lesson/LessonCanvasScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LessonCanvasScreen extends StatefulWidget {
+  const LessonCanvasScreen({super.key});
+
+  @override
+  State<LessonCanvasScreen> createState() => _LessonCanvasScreenState();
+}
+
+class _LessonCanvasScreenState extends State<LessonCanvasScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/lesson/LessonScreen.dart
+++ lib/lesson/LessonScreen.dart
@@ -2,14 +2,9 @@ import 'package:blueberry_flutter_template/lesson/LessonListView.dart';
 import 'package:blueberry_flutter_template/modules/core/utils/AppStrings.dart';
 import 'package:flutter/material.dart';
 
-class LessonScreen extends StatefulWidget {
+class LessonScreen extends StatelessWidget {
   const LessonScreen({super.key});
 
-  @override
-  State<LessonScreen> createState() => _LessonScreenState();
-}
-
-class _LessonScreenState extends State<LessonScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
