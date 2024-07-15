e63ec7c0d62a947555ffb9c92a9858b10df91028
Jungwoo <108061510+jwson-automation@users.noreply.github.com>
2024-07-15 12:35:12 +0900

    DogProfileModel to Freezed

--- lib/screens/match/MatchScreen.dart
+++ lib/screens/match/MatchScreen.dart
@@ -11,6 +11,34 @@ import 'ProfileScreen.dart';
 ///  - CardSwiper: 프로필 카드를 스와이프할 수 있는 메인 위젯
 ///  - FloatingActionButton: 수동으로 좌/우 스와이프를 할 수 있는 버튼
 
+
+final List<DogProfileModel> dogProfiles = [
+  DogProfileModel(
+    name: '멍멍이',
+    gender: '수컷',
+    breed: '골든 리트리버',
+    location: '서울',
+    bio: '산책을 좋아하는 활발한 강아지예요!',
+    imageUrl: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcY8RCd%2FbtsC31gfzPL%2FbSnADONPu66xPesaWHmW00%2Fimg.jpg',
+  ),
+  DogProfileModel(
+    name: '뭉치',
+    gender: '암컷',
+    breed: '푸들',
+    location: '부산',
+    bio: '애교 많고 사랑스러운 강아지입니다.',
+    imageUrl: 'https://image.dongascience.com/Photo/2017/07/14994185580021.jpg',
+  ),
+  DogProfileModel(
+    name: '초코',
+    gender: '수컷',
+    breed: '비글',
+    location: '대구',
+    bio: '장난기 많고 활발한 성격이에요.',
+    imageUrl: 'https://cdn.newscj.com/news/photo/201309/170126_122268_2013.jpg',
+  ),
+];
+
 class MatchScreen extends StatefulWidget {
   const MatchScreen({super.key});
 
