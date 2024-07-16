8befabfb7d3c63c2d8883f25a41b55310b799ae4
JaeHo Song <thruthesky@gmail.com>
2024-07-16 11:14:54 +0800

    removing unwanted code

--- lib/screens/mypage/MyPageScreen.dart
+++ lib/screens/mypage/MyPageScreen.dart
@@ -168,25 +168,6 @@ class MyPageScreen extends ConsumerWidget {
                 ),
               )),
             ),
-
-            ElevatedButton(
-              onPressed: () async {
-                // Trigger the sign-in flow
-                final LoginResult loginResult =
-                    await FacebookAuth.instance.login();
-
-                // Create a credential from the access token
-                final OAuthCredential facebookAuthCredential =
-                    FacebookAuthProvider.credential(
-                  loginResult.accessToken!.token,
-                );
-
-                // Once signed in, return the UserCredential
-                FirebaseAuth.instance
-                    .signInWithCredential(facebookAuthCredential);
-              },
-              child: const Text('Facebook Login'),
-            ),
           ],
         ),
       ),
